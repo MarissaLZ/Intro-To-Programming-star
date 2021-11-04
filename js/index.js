32d02a219f2b410989f15702a33f148dbdb6356e
Mlazo4 <Mlazo4@csgofoodreviews.com>
2021-11-03 21:45:18 -0400

    adding form and message list

--- js/index.js
+++ js/index.js
@@ -1,27 +1,47 @@
+//displays copyright
 var today = new Date();
 var thisYear = today.getFullYear();
 //use querySelector to access by tag name
-var footer= document.querySelector('footer');
-//console.log(footer)
-
-var copyright = document.createElement('p')
-copyright.innerHTML = 'Marissa Lazo ' + thisYear;
-//console.log(copyright)
+var footer=document.querySelector('footer');
+var copyright=document.createElement('p')
+copyright.innerHTML='Marissa Lazo ' + thisYear;
 footer.append(copyright);
 
-//why doesn't this work?
-//footer.appendChildcopyright)
-//index.js:11 Uncaught TypeError: Failed to execute 'appendChild' on 
-//'Node': parameter 1 is not of type 'Node'.at index.js:11
+//lists skills section
+var skills = ["Python", "JavaScript", "Java"];
+var skillsSelection=document.querySelector('#skills');
+var skillsList=skillsSelection.querySelector('ul');
 
-var skills = ["Python", "JavaScript", "Java"]
-var skillsSelection = document.querySelector('#skills')
-var skillsList = skillsSelection.querySelector('ul')
-
-//loops by index
 for (let i in skills) {
-    var skill = document.createElement('li')
-    skill.innerHTML= skills[i]
-    skillsList.append(skill) 
+    var skill=document.createElement('li')
+    skill.innerHTML=skills[i];
+    skillsList.append(skill) ;
 }
+//Message form
+//note .value gives us the value attribute. Once there is user input the element has a value attribute
+var messageForm=document.querySelector('form')
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault(); //use messageForm?
+    var nameInput=event.target.name.value; 
+    var emailInput=event.target.mail.value; //how is this being accesed? by id? works w/email
+    var messageInput=event.target.message.value;
+    console.log(`${nameInput} ${emailInput} ${messageInput}`)
 
+    var messageSection=document.querySelector('#messages')
+    var messageList=messageSection.querySelector('ul')
+    var newMessage=document.createElement('li')
+    //Is there a better way to put a space between the message and button?
+    newMessage.innerHTML=`<a href=mailto:${emailInput}>${nameInput} wrote: </a><br><span>${messageInput} </span>`
+    var removeButton=document.createElement('button')
+    removeButton.innerHTML='remove'
+    removeButton.type='button'
+    
+    removeButton.addEventListener('click', (event) => {
+        var entry=removeButton.parentNode
+        entry.remove()
+    });
+    newMessage.appendChild(removeButton)
+    messageList.appendChild(newMessage)
+
+    messageForm.reset()
+});
