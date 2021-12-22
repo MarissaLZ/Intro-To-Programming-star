6d3357b2f8b64fe54eac372ecd2f66801c24fcd1
Mlazo4 <Mlazo4@csgofoodreviews.com>
2021-12-21 22:15:38 -0500

    adds additional styling

--- js/index.js
+++ js/index.js
@@ -4,64 +4,83 @@ let thisYear = today.getFullYear();
 //use querySelector to access by tag name
 let footer = document.querySelector('footer');
 let copyright = document.createElement('p');
-copyright.innerHTML= '&copy Marissa Lazo ' + thisYear;
+copyright.innerHTML = '&copy Marissa Lazo ' + thisYear;
 footer.append(copyright);
 
 //lists skills section
 let skills = ["HTML", "CSS", "JS" ,"Python", "Java", "GitHub"];
-let skillsSelection=document.querySelector('#skills');
-let skillsList=skillsSelection.querySelector('ul');
+let skillsSelection = document.querySelector('#skills');
+let skillsList = skillsSelection.querySelector('ul');
 
 for (let i in skills) {
     let skill = document.createElement('li')
     skill.innerHTML = skills[i];
     skillsList.append(skill) ;
 }
+//sample message
+let sampleMessageList = document.querySelector('#messages').querySelector("ul")
+let sampleMessage = document.createElement('li');
+sampleMessage.innerHTML = `<a href=mailto:Lazomarissa@gmail.com>Marissa wrote: </a><br><span>Hey everyone! Leave a message using the form above.</span>`;
+sampleMessageList.appendChild(sampleMessage);
+
 //Message form
 //note .value gives us the value attribute. Once there is user input the element has a value attribute
-let messageForm=document.querySelector('form');
+let messageForm = document.querySelector('form');
 messageForm.addEventListener('submit', (event) => {
-    event.preventDefault(); //use messageForm?
-    let nameInput=event.target.name.value; 
-    let emailInput=event.target.mail.value; //how is this being accesed? by id? works w/email
-    let messageInput=event.target.message.value;
-    console.log(`${nameInput} ${emailInput} ${messageInput}`);
+    event.preventDefault(); 
+    let nameInput = event.target.name.value; 
+    let emailInput = event.target.mail.value; 
+    let messageInput = event.target.message.value;
 
-    let messageSection=document.querySelector('#messages');
-    let messageList=messageSection.querySelector('ul');
-    let newMessage=document.createElement('li');
+    sampleMessage.remove();
+    let messageList = document.querySelector('#messages').querySelector('ul');
+    let newMessage = document.createElement('li');
     
-    newMessage.innerHTML=`<a href=mailto:${emailInput}>${nameInput} wrote: </a><br><span>${messageInput} </span>`;
-    let removeButton=document.createElement('button');
-    removeButton.innerHTML='Remove';
-    removeButton.type='button';
+    newMessage.innerHTML =`<a href=mailto:${emailInput}>${nameInput} wrote: </a><br><span>${messageInput}</span>`;
+    let removeButton = document.createElement('button');
+    removeButton.innerHTML = 'Remove';
+    removeButton.type = 'button';
     
     removeButton.addEventListener('click', (event) => {
-        let entry=removeButton.parentNode;
+        let entry = removeButton.parentNode;
         entry.remove();
     });
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
-
     messageForm.reset();
 });
 
 //fetch request
 //returns a promise
-
 fetch("https://api.github.com/users/MarissaLZ/repos")
-.then(response=> response.json())
+.then(response => response.json())
 .then(function(data) {
     var projectList = document.getElementById("projects").querySelector("ul")
-    console.log(data)
 
-        for (i in data) {
-            var link = document.createElement("a")
-            link.setAttribute("href",data[i].html_url);
-            link.setAttribute("target", "blank")
-            link.innerHTML = data[i].name;
-            var project = document.createElement("li");
-            project.appendChild(link)
-            projectList.appendChild(project)
-        }
+    for (i in data) {
+        //get repo name
+        let projectName = document.createElement("h3")
+        projectName.innerHTML = data[i].name;
+        projectName.className ="projectTags";
+
+        //get repo description
+        let projectDescription = document.createElement("p")
+        projectDescription.innerHTML = data[i].description;
+        projectDescription.className ="projectTags";
+        
+        ///create a project 
+        let project = document.createElement("li");
+
+        //append name and description
+        project.appendChild(projectName)
+        project.appendChild(projectDescription);
+
+        //create clickable section
+        let link = document.createElement("a");
+        link.setAttribute("href",data[i].html_url);
+        link.setAttribute("target", "blank")
+
+        link.appendChild(project)
+        projectList.appendChild(link)
+    }
     })
